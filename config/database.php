397e019b4afaf9e2d6b691af7f9b9310e9bf3452
hnck66-code <hnck66@gmail.com>
2025-11-17 21:05:34 +0700

    Update database.php

--- config/database.php
+++ config/database.php
@@ -1,12 +1,27 @@
 <?php
-// config/database.php
+
+// Jika berjalan di GitHub Actions (CI), jangan connect database
+if (getenv('CI') === 'true') {
+    // Return dummy connection
+    return [
+        'connection' => null
+    ];
+}
+
+// Normal di localhost XAMPP
 $host = "localhost";
 $user = "root";
 $pass = "";
 $db   = "db_login";
 
 $conn = new mysqli($host, $user, $pass, $db);
+
 if ($conn->connect_error) {
     die("Koneksi database gagal: " . $conn->connect_error);
 }
+
 $conn->set_charset("utf8mb4");
+
+return [
+    'connection' => $conn
+];
